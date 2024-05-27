eb219d65a1e9dbfc4303249e9e2829b275278db1
Legion\PC-KC <killercoderpt@gmail.com>
2024-05-27 12:57:22 +0100

    Load casino penthouse ipl and interior

--- client/index.ts
+++ client/index.ts
@@ -1,3 +1,39 @@
 import * as alt from 'alt-client';
+import * as native from 'natives';
+import { LoaderIPLConfig } from '../shared/config.js';
+
+
+alt.requestIpl('vw_casino_penthouse');
+let interiorID = native.getInteriorAtCoords(976.6364, 70.29476, 115.1641);
+if (native.isValidInterior(interiorID)) {
+    native.activateInteriorEntitySet(interiorID, 'Set_Pent_Tint_Shell');
+    native.activateInteriorEntitySet(interiorID, 'Set_Pent_Pattern_09');
+    native.activateInteriorEntitySet(interiorID, 'Set_Pent_Spa_Bar_Open');
+    native.activateInteriorEntitySet(interiorID, 'Set_Pent_Media_Bar_Open');
+    native.activateInteriorEntitySet(interiorID, 'Set_Pent_Arcade_Modern');
+    native.activateInteriorEntitySet(interiorID, 'Set_Pent_Bar_Clutter');
+    native.activateInteriorEntitySet(interiorID, 'Set_Pent_Clutter_03');
+    native.activateInteriorEntitySet(interiorID, 'Set_pent_bar_light_02');
+    
+    native.setInteriorEntitySetTintIndex(interiorID, 'Set_Pent_Tint_Shell', 1);
+    native.setInteriorEntitySetTintIndex(interiorID, 'Set_Pent_Pattern_09', 1);
+    native.setInteriorEntitySetTintIndex(interiorID, 'Set_Pent_Spa_Bar_Open', 1);
+    native.setInteriorEntitySetTintIndex(interiorID, 'Set_Pent_Media_Bar_Open', 1);
+    native.setInteriorEntitySetTintIndex(interiorID, 'Set_Pent_Arcade_Modern', 1);
+    native.setInteriorEntitySetTintIndex(interiorID, 'Set_Pent_Bar_Clutter', 1);
+    native.setInteriorEntitySetTintIndex(interiorID, 'Set_Pent_Clutter_03', 1);
+    native.setInteriorEntitySetTintIndex(interiorID, 'Set_pent_bar_light_02', 1);
+
+    native.refreshInterior(interiorID);
+}
+
+alt.requestIpl('vw_casino_main');
+alt.requestIpl('vw_casino_carpark');
+alt.requestIpl('vw_dlc_casino_door');
+alt.requestIpl('vw_casino_door');
+alt.requestIpl('hei_dlc_windows_casino');
+alt.requestIpl('hei_dlc_casino_door');
+alt.requestIpl('hei_dlc_casino_aircon');
+alt.requestIpl('vw_casino_garage');
 
 alt.loadDefaultIpls();
--- shared/config.ts
+++ shared/config.ts
@@ -1,5 +1,49 @@
 export const LoaderIPLConfig = {
     iplList: {
-        gtav: []
+        gtav: [],
+        casino: [
+            'vw_casino_penthouse',
+            'hei_dlc_windows_casino',
+            'vw_casino_carpark',
+            'vw_casino_garage',
+            'vw_casino_main'
+        ],
+        casinoInterior: [
+            'Set_Pent_Tint_Shell',
+            'Set_Pent_Pattern_01',
+            'Set_Pent_Pattern_02',
+            'Set_Pent_Pattern_03',
+            'Set_Pent_Pattern_04',
+            'Set_Pent_Pattern_05',
+            'Set_Pent_Pattern_06',
+            'Set_Pent_Pattern_07',
+            'Set_Pent_Pattern_08',
+            'Set_Pent_Pattern_09',
+            'Set_Pent_Spa_Bar_Open',
+            'Set_Pent_Spa_Bar_Closed',
+            'Set_Pent_Media_Bar_Open',
+            'Set_Pent_Media_Bar_Closed',
+            'Set_Pent_Dealer',
+            'Set_Pent_NoDealer',
+            'Set_Pent_Arcade_Retro',
+            'Set_Pent_Arcade_Modern',
+            'Set_Pent_Bar_Clutter',
+            'Set_Pent_Clutter_01',
+            'Set_Pent_Clutter_02',
+            'Set_Pent_Clutter_03',
+            'set_pent_bar_light_0',
+            'set_pent_bar_light_01',
+            'set_pent_bar_light_02',
+            'set_pent_bar_party_0',
+            'set_pent_bar_party_1',
+            'set_pent_bar_party_2',
+            'set_pent_bar_party_after',
+            'Set_Pent_GUEST_BLOCKER',
+            'Set_Pent_LOUNGE_BLOCKER',
+            'Set_Pent_OFFICE_BLOCKER',
+            'Set_Pent_CINE_BLOCKER',
+            'Set_Pent_SPA_BLOCKER',
+            'Set_Pent_BAR_BLOCKER'
+        ]
     }
 }
