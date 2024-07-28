9abd9ff99761ee7174a67e25863136656e19de8d
KC0d <killer-coder@hotmail.com>
2024-07-28 04:27:23 +0100

    added Simons Car Dealer interior

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,8 @@
 import * as alt from 'alt-server';
 
-// Index.ts Is Required
+// alt.on('playerConnect', (player: alt.Player) => {
+
+//     player.emit('IPLLoader::Add', 'smboat', 'SM Boat', -2027.946, -1036.695, 6.707587, false);
+//     player.emit('IPLLoader::Add', '', 'Bunker Interior', 899.5518, -3246.038, -98.04907, false);
+
+// });
